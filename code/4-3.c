10e7443ca918389277993388ddb7050336393d17
gdy <153972304+wyhoaule@users.noreply.github.com>
2026-04-09 17:07:43 +0900

    4-3.c

---
+++ code/4-3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int arr[5] = {10, 20, 30, 40, 50};
+  int size = 5;
+  int index;
+
+  scanf("%d)", &index);
+  if (index >= 0 && index < size) {
+        printf("%d\n", arr[index]);
+    } else {
+        printf("Error!\n");
+    }
+  return 0;
+}
